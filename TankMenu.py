9cb2a4acd1871558ca073df1d1faf4302c5aea52
GTE86 <tgun0009@student.monash.edu>
2022-04-25 19:37:17 +1000

    Added Temp stuff + revision to ultrasonic bits

--- TankMenu.py
+++ TankMenu.py
@@ -1,4 +1,3 @@
-from re import I
 import time
 from tkinter import Image
 import matplotlib.pyplot as plt
@@ -6,6 +5,8 @@ from PIL import Image
 from pymata4 import pymata4
 
 pin = 0000
+temp = [12, 10, 9, 19, 22, 22, 18, 15]
+timePoints = [200, 500, 800, 1100, 1400, 1700, 2000, 2300]
 
 def pin_entry():
     global pin
@@ -32,28 +33,37 @@ def display_main_menu():
     print("\033[0;37;40m Select one of the following, with the number noted.")
     print("1. View current state.")
     print("2. View previous graphs.")
-    print("3. Graph Generation.")
+    print("3. Distance Graph Generation.")
+    print("4: Temperature Entry (placeholder).")
+    print("5: Temperature Graph Generation (placeholder).")
+    print("0: Escape.")
 
     choice = -1
 
     while True:
         try:
             choice = int(input("Make your decision. "))
-            if choice == 1 or choice == 2 or choice == 3:
+            if choice == 0 or choice == 1 or choice == 2 or choice == 3 or choice == 4 or choice == 5:
                 break;
             else:
                 print("Make sure to choose only from the following.")
         except ValueError:
-            print("\033[2;31;40m Remember to choose from the following, by the number specified on the left.]")
+            print("\033[2;31;40m Remember to choose from the following, by the number specified on the left.")
 
     if choice == 1:
-        view_current()
+        dview_current()
     elif choice == 2:
-        view_graph()
+        dview_graph()
     elif choice == 3:
-        graph_generation()
+        dgraph_generation()
+    elif choice == 4:
+        temp_entry()
+    elif choice == 5:
+        tgraph_generation()
+    elif choice == 0:
+        end_program()
 
-def view_current():
+def dview_current():
     board = pymata4.Pymata4()
     triggerPin = 9
     echoPin = 10
@@ -80,12 +90,12 @@ def view_current():
     
     sonar_setup(board, triggerPin, echoPin)
 
-def view_graph():
+def dview_graph():
     i = int(input("Which Graph Iteration would you like to view? (denote with number of iteration): "))
     image = Image.open(f'Distance_Graph_{i}.png')
     image.show()
 
-def graph_generation():
+def dgraph_generation():
     ypoint = []
     xpoint = []
 
@@ -129,6 +139,42 @@ def graph_generation():
                 board.shutdown()
 
     sonar_setup(board, triggerPin, echoPin)
+
+def temp_entry():
+    global temp
+    global timePoints
+
+    newEntry = int(input("Enter new Ambient temperature: "))
+    temp.append(newEntry)
+    newTime = int(input("Enter current time (HHMM): "))
+    timePoints.append(newTime)
+
+    print("Current temp values stored: ", temp)
+    print("Correspoding time values: ", timePoints)
+    display_main_menu()
+
+def tgraph_generation():
+    global temp
+    global timePoints
+    timePoints.sort()
+    t = 0
+    while True:
+        try:
+            t = t + 1
+            plt.scatter(timePoints, temp, marker = '.', color = 'blue')
+            plt.ylabel("Temperature (°C)")
+            plt.xlabel("Time (HHMM)")
+            plt.title(f"Graph iteration for distance: {t}")
+            plt.savefig(f"Temperature_Graph_{t}.png")
+        except Exception:
+            print("error...")
+        
+        display_main_menu()
+
+def end_program():
+    print("System shutdown.")
+    quit()
+
 
 name = input("Welcome. Please Enter your name: ")
 print("Hello,",name,". Nice to see you!")
