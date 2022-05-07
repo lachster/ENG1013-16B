8c3d0d069dae638c90f215c86f965ea4d230fbb0
lachster <104074266+lachster@users.noreply.github.com>
2022-05-07 12:57:18 +1000

    Update buzzer.py

--- Milestone 3/buzzer.py
+++ Milestone 3/buzzer.py
@@ -13,11 +13,20 @@ pwmbuzzerpin = 9
 
 myArduino.set_pin_mode_digital_output(buzzerPin)
 myArduino.set_pin_mode_pwm_output(pwmbuzzerpin)
+
+
 def buzzer(duration):
     myArduino.digital_pin_write(buzzerPin,1)
     time.sleep(duration)
     myArduino.digital_pin_write(buzzerPin,0)
 
 
-def pwm_buzzer(frequency):
-    myArduino.pwm_write(pwmbuzzerpin,frequency)
+def pwm_buzzer(duration):
+    myArduino.pwm_write(pwmbuzzerpin,1)
+    time.sleep(duration)
+    myArduino.pwm_write(pwmbuzzerpin,0)
+
+    
+buzzer(5)
+
+pwm_buzzer(5)
